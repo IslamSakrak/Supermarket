0df2ec52dfa91f26cd514a16f91371fea66ad9a0
Islam <islamsakrak2002@gmail.com>
2022-04-07 18:20:10 +0200

    Improving the naming , removing comments and adjusting methods

--- src/main/java/Supermarket.java
+++ src/main/java/Supermarket.java
@@ -1,32 +1,28 @@
 import java.io.*;
 import java.util.*;
+
 public class Supermarket {
-    private static final Random random = new Random();
+
     private static final Map<String, Integer> priceByProducts = new HashMap<>();
 
-    public void transaction() {
+    public void shoppingCartItems() {
         getShoppingCartItems();
-        System.out.println("There are " + priceByProducts.size() + " items in the cart.");
-        List<Integer> values = priceByProducts.values().stream().toList();
-        int randomShoppingCartItemPrice = getRandomItem(values);
-        int sum = getPriceSum(randomShoppingCartItemPrice);
-        System.out.println("Which is a total of $" + sum + " dollars.");
+        System.out.println("There are " + priceByProducts.size() + " items in the cart.\nWhich is a total of $" + getProductSum() + " dollars."); // outputs the number of products
     }
 
-    private int getPriceSum(int randomValue) {
+    public int getRandomNumber() {
+        int MAX_PRODUCT_PRICE = 100;
+        return new Random().nextInt(1, MAX_PRODUCT_PRICE);
+    }
+
+    private int getProductSum() {
         int sum = 0;
-        /*
-        valuesByKeys as in teamsByCaptains. If you are going to include both key and value, this seems to read best.
-         At a high level, you can read it as just teams”, so anything that's performed on it is being performed on teams.
-          The byCaptains prefix reads as it should do: a less significant qualifier that follows the teams around to help
-           someone understands the structure if they need to.
-         */
         for (Map.Entry<String, Integer> priceByProduct : priceByProducts.entrySet()) {
-            printMapElements(priceByProduct);
-            if (isProductOnDiscount() && randomValue == priceByProduct.getValue()) {
+            printProducts(priceByProduct);
+            if (isProductOnDiscount() && isPickedForPriceChange(priceByProduct)) {
                 priceDiscountText(priceByProduct);
                 sum += getDiscount(priceByProduct);
-            } else if (isProductPriceIncrease() && randomValue == priceByProduct.getValue()) {
+            } else if (isProductPriceIncrease() && isPickedForPriceChange(priceByProduct)) {
                 priceIncreaseText(priceByProduct);
                 sum += getPriceIncrease(priceByProduct);
             } else {
@@ -35,8 +31,13 @@ public class Supermarket {
         }
         return sum;
     }
-                                                
-    private void printMapElements(Map.Entry<String, Integer> shoppingCartElement) {
+
+
+    private boolean isPickedForPriceChange(Map.Entry<String, Integer> priceByProduct) {
+        return getRandomNumber() == priceByProduct.getValue();
+    }
+
+    private void printProducts(Map.Entry<String, Integer> shoppingCartElement) {
         System.out.println(shoppingCartElement.getKey() + ":" + shoppingCartElement.getValue());
     }
 
@@ -50,45 +51,38 @@ public class Supermarket {
                 + (int) (shoppingCartItem.getValue() * 0.7));
     }
 
-    private double getPriceIncrease(Map.Entry<String, Integer> shoppingCartItem) {
-        return shoppingCartItem.getValue() * 1.1;
+    private int getPriceIncrease(Map.Entry<String, Integer> shoppingCartItem) {
+        return (int) (shoppingCartItem.getValue() * 1.1);
     }
 
-    private double getDiscount(Map.Entry<String, Integer> shoppingCartItem) {
-        return shoppingCartItem.getValue() * 0.7;
-    }
-
-    private int getRandomItem(List<Integer> values) {
-        return values.get(random.nextInt(values.size()));
+    private int getDiscount(Map.Entry<String, Integer> shoppingCartItem) {
+        return (int) (shoppingCartItem.getValue() * 0.7);
     }
 
     private void getShoppingCartItems() {
-        try (BufferedReader br = getBufferedReader();) {
+        try (BufferedReader br = getBufferedReader()) {
             for (String line; (line = br.readLine()) != null; ) {
                 addToShoppingCart(line);
             }
         } catch (IOException e) {
-            System.out.println("Something is wrong with the file");
+            System.out.println(e.getCause().getMessage());
         }
     }
 
     private BufferedReader getBufferedReader() throws IOException {
         File file = new File("src/main/resources/groceriesList.txt");
-        FileReader fileReader = new FileReader(file);
-        return new BufferedReader(fileReader);
+        return new BufferedReader(new FileReader(file));
     }
 
     private void addToShoppingCart(String line) {
-        priceByProducts.putIfAbsent(line, random.nextInt(1,100));
+        priceByProducts.putIfAbsent(line, getRandomNumber());
     }
 
     private boolean isProductOnDiscount() {
-        double d = Math.random();
-        return d < 0.5;
+        return Math.random() < 0.8;
     }
 
     private boolean isProductPriceIncrease() {
-        double d = Math.random();
-        return d < 0.2;
+        return Math.random() < 0.3;
     }
 }
